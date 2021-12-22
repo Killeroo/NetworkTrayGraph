f85c1cf380ad4f6af3619c7605e3f8c6363e62d5
Matthew Carney <matthewcarney64@gmail.com>
2021-12-22 01:31:39 +0000

    Initial commit

---
+++ Chasovoy/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chasovoy
+{
+    public static class Helper
+    {
+        public static float Map(float value, float valueMin, float valueMax, float outMin, float outMax)
+        {
+            return (value - valueMin) / (valueMax - valueMin) * (outMax - outMin) + outMin;
+        }
+    }
+}
---
+++ Chasovoy/NetworkMonitor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Collections.Generic;
+
+namespace Chasovoy
+{
+    public class NetworkMonitor
+    {
+        public Dictionary<string, (long recv, long sent, long lastRecv, long lastSent)> InterfaceStats { get; set; } =
+            new Dictionary<string, (long recv, long sent, long lastRecv, long lastSent)>();
+
+        public NetworkMonitor() { }
+
+        public void Update()
+        {
+            // TODO: Check periodically if new network interfaces have been added
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                long recv = nic.GetIPv4Statistics().BytesReceived;
+                long sent = nic.GetIPv4Statistics().BytesSent;
+                string name = nic.Name;
+
+                if (!InterfaceStats.ContainsKey(nic.Name))
+                {
+                    InterfaceStats.Add(
+                        nic.Name,
+                        (recv, sent, 0, 0)
+                    );
+                }
+                else
+                {
+                    var values = InterfaceStats[name];
+                    values.lastRecv = InterfaceStats[name].recv;
+                    values.lastSent = InterfaceStats[name].sent;
+                    values.sent = sent;
+                    values.recv = recv;
+                    InterfaceStats[name] = values; // Don't think this is really needed
+
+                }
+            }
+        }
+
+    }
+}
